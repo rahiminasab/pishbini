e797e890801325cd99676960d47313bc161c8204
Ehsan RahimiNasab <rahiminasabe@gmail.com>
2018-07-02 07:22:40 -0400

    scoring fixed

--- pishi/models.py
+++ pishi/models.py
@@ -81,26 +81,37 @@ class Match(models.Model):
     def due(self):
         return datetime.now(pytz.UTC) >= self.date
 
-    def get_winner(self):
+    def get_winner_in_120(self):
+        if self.home_result is None or self.away_result is None:
+            raise ValueError("You cannot know who is winner of the match %s "
+                             "while you didn't input the final results" % self)
         if self.home_result > self.away_result:
             return self.home_team
         elif self.home_result == self.away_result:
-            if not self.home_penalty:
-                return None
-            elif self.home_penalty > self.away_penalty:
-                return self.home_team
-            else:
-                return self.away_team
+            return None
+        else:
+            return self.away_team
+
+    def has_penalty(self):
+        return self.home_penalty is not None and self.away_penalty is not None
+
+    def get_winner_in_penalty(self):
+        if self.get_winner_in_120():
+            return None
+        if not self.has_penalty():
+            raise ValueError("Match %s, does not lead into penalties, but you have requested for penalties." % self)
+        if self.home_penalty > self.away_penalty:
+            return self.home_team
         else:
             return self.away_team
 
     def save(self, *args, **kwargs):
-        if self.finished and not self.exceptional_badge:
-            self.winner = self.get_winner()
+        if self.finished and self.exceptional_badge is None:
+            self.winner = self.get_winner_in_120()
             tot_count = Predict.objects.filter(match=self).count()
             if tot_count > 0:
                 err_count = Predict.objects.filter(match=self).exclude(winner=self.winner).count()
-                p_val = (tot_count-err_count)/float(tot_count)
+                p_val = (tot_count - err_count) / float(tot_count)
                 if p_val < 0.1:
                     self.exceptional_badge = Badge.ORACLE
                 elif p_val < 0.2:
@@ -112,7 +123,8 @@ class Match(models.Model):
             else:
                 self.exceptional_badge = Badge.NOTHING
 
-            self.summary = MatchSummary.objects.create()
+            if not self.summary.pk:
+                self.summary = MatchSummary.objects.create()
 
             super(Match, self).save(*args, **kwargs)
 
@@ -141,6 +153,13 @@ class MatchSummary(models.Model):
     nostradamuses = models.PositiveIntegerField(default=0)
     trelawneies = models.PositiveIntegerField(default=0)
 
+    def delete(self, *args, **kwargs):
+        self.match.match_set.summary.royals -= self.royals
+        self.match.match_set.summary.full_houses -= self.full_houses
+        self.match.match_set.summary.straights -= self.straights
+        self.match.match_set.summary.one_pairs -= self.one_pairs
+        super(MatchSummary, self).delete(*args, **kwargs)
+
 
 class MatchSetSummary(models.Model):
     royals = models.PositiveIntegerField(default=0)
@@ -151,47 +170,47 @@ class MatchSetSummary(models.Model):
 
 class Predict(models.Model):
     user = models.ForeignKey(User, related_name="predictions", on_delete=models.CASCADE)
-    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    match = models.ForeignKey(Match, related_name="predictions", on_delete=models.CASCADE)
     home_result = models.PositiveIntegerField()
     away_result = models.PositiveIntegerField()
     home_penalty = models.PositiveIntegerField(null=True, blank=True)
     away_penalty = models.PositiveIntegerField(null=True, blank=True)
     winner = models.ForeignKey(Team, null=True, blank=False)
     normal_badge = models.PositiveIntegerField(choices=Badge.normal_types, null=True, blank=True)
+    penalty_badge = models.BooleanField(blank=True)
     exceptional_badge = models.PositiveIntegerField(choices=Badge.exceptional_types, null=True, blank=True)
 
     class Meta:
         unique_together = ('user', 'match',)
 
-    def get_winner(self):
+    def get_winner_in_120(self):
+        if self.home_result is None or self.away_result is None:
+            raise ValueError("You cannot know who is winner of the predict %s "
+                             "while you didn't input the final results" % self)
+
         if self.home_result > self.away_result:
             return self.match.home_team
         elif self.home_result == self.away_result:
-            if not self.home_penalty:
-                return None
-            elif self.home_penalty > self.away_penalty:
-                return self.match.home_team
-            else:
-                return self.match.away_team
+            return None
+        else:
+            return self.match.away_team
+
+    def get_winner_in_penalty(self):
+        if self.get_winner_in_120():
+            return None
+        if self.home_penalty is None or self.away_penalty is None:
+            raise ValueError("Predict %s, does not have a prediction of penalties, "
+                             "but you have requested for penalties winner." % self)
+        if self.home_penalty > self.away_penalty:
+            return self.match.home_team
         else:
             return self.match.away_team
 
     def is_royal(self):
-        if self.is_straight():
-            if self.home_result == self.match.home_result and self.away_result == self.match.away_result:
-                if self.match.home_penalty and self.match.winner == self.match.home_team:
-                    return self.home_penalty == self.match.home_penalty
-                elif self.match.away_penalty and self.match.winner == self.match.away_team:
-                    return self.away_penalty == self.match.away_penalty
-                return True
-        return False
+        return self.home_result == self.match.home_result and self.away_result == self.match.away_result
 
     def is_full_house(self):
         if self.is_straight():
-            if self.home_result == self.away_result and self.match.home_result == self.match.away_result:
-                if self.match.home_penalty:
-                    return self.home_penalty - self.away_penalty == self.match.home_penalty - self.match.away_penalty
-                return True
             return self.home_result - self.away_result == self.match.home_result - self.match.away_result
         return False
 
@@ -201,17 +220,16 @@ class Predict(models.Model):
     def value(self):
         if not self.match.finished:
             return 0
-        return Badge.get_value(self.normal_badge) + Badge.get_value(self.exceptional_badge)
+        return Badge.get_value(self.normal_badge) + Badge.get_value(self.exceptional_badge) + \
+               (5 if self.penalty_badge else 0)
 
     def save(self, *args, **kwargs):
-        if not Score.objects.filter(user=self.user, match_set=self.match.match_set).exists():
-            Score.objects.create(user=self.user, match_set=self.match.match_set)
-        if self.home_result != self.away_result:
-            self.home_penalty = None
-            self.away_penalty = None
-        self.winner = self.get_winner()
-        if not self.normal_badge and self.match.finished:
+        if not self.pk:
+            self.winner = self.get_winner_in_120()
+            if not Score.objects.filter(user=self.user, match_set=self.match.match_set).exists():
+                Score.objects.create(user=self.user, match_set=self.match.match_set)
 
+        if self.normal_badge is None and self.match.finished:
             if self.is_royal():
                 self.normal_badge = Badge.ROYAL
                 self.match.summary.royals += 1
@@ -224,6 +242,10 @@ class Predict(models.Model):
             else:
                 self.normal_badge = Badge.ONE_PAIR
                 self.match.summary.one_pairs += 1
+
+            if self.winner is None and self.match.has_penalty():
+                if self.get_winner_in_penalty() == self.match.get_winner_in_penalty():
+                    self.penalty_badge = True
 
             if self.is_straight():
                 self.exceptional_badge = self.match.exceptional_badge
@@ -247,7 +269,8 @@ class Predict(models.Model):
                 score_obj.num_predicted += 1
             except Score.DoesNotExist:
                 score_obj = Score(user=predict.user, match_set=finished_match.match_set, num_predicted=1)
-            score_obj.value += predict.value() * (2**finished_match.type)
+            val = predict.value()
+            score_obj.value += val
             score_obj.save()
         match_set_summary = finished_match.match_set.summary
         match_set_summary.royals += finished_match.summary.royals
